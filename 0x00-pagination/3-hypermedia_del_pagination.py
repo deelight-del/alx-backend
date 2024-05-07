8b9d84d0febd75b49072401f6db7c47d55add449
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-07 09:11:04 +0000

    document method

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """The get_hyper_index implementation to be robust
+        against deletion"""
         assert index <= max(self.indexed_dataset().keys())
         return {
             "index": index,
