62752339208625a13f44a17c1fd7883c340686e7
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-14 17:31:31 +0000

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This module implements a flask application"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """The configuration class for our application"""
+    LANGUAGES = ["en", "fr"]
+    app.config["BABEL_DEFAULT_LOCALE"] = "en"
+    app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
+
+
+@app.route("/")
+def hello_world():
+    """Renders html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
