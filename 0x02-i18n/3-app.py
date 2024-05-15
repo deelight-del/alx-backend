038042f45e23c29c5024c71823a8543d4011d1cf
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-15 05:27:16 +0000

    strict slashes implementation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
     """Renders html template"""
     return render_template('3-index.html')
